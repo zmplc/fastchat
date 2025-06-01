d21aac1e79bccbf6811458ac22d797031f2a212e
zmplc <39064128+zmplc@users.noreply.github.com>
2025-06-01 16:05:27 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
+  $hostname = "";
+  $username = "";
   $password = "";
-  $dbname = "fastchat";
+  $dbname = "";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
